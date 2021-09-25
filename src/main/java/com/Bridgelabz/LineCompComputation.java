94ae2ee46522052ed38e9d088b9fac7c9b38d9ed
Akshay <akshay@gmail.com>
2021-09-25 11:33:47 +0530

    Initial Commit

---
+++ src/main/java/com/Bridgelabz/LineCompComputation.java
@@ -0,0 +1,26 @@
+package com.Bridgelabz;
+
+import java.util.Scanner;
+
+public class LineCompComputation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter X co-ordinator of 1st point: ");
+        int X1 = sc.nextInt();
+        System.out.println("Enter Y co-ordinator of 1st point: ");
+        int Y1 = sc.nextInt();
+        System.out.println("Enter X co-ordinator of 2nd point: ");
+        int X2 = sc.nextInt();
+        System.out.println("Enter Y co-ordinator of 2nd point: ");
+        int Y2 = sc.nextInt();
+
+        System.out.println("Enter X co-ordinator of 1st point: ");
+        int X3 = sc.nextInt();
+        System.out.println("Enter Y co-ordinator of 1st point: ");
+        int Y3 = sc.nextInt();
+        System.out.println("Enter X co-ordinator of 2nd point: ");
+        int X4 = sc.nextInt();
+        System.out.println("Enter Y co-ordinator of 2nd point: ");
+        int Y4 = sc.nextInt();
+    }
+}
