425a8991d588430a88d6e5cb8d476e4177e77bf6
Akshay <akshay@gmail.com>
2021-09-25 16:18:28 +0530

    Added compareTo method in the program

--- src/main/java/com/Bridgelabz/LineCompComputation.java
+++ src/main/java/com/Bridgelabz/LineCompComputation.java
@@ -3,6 +3,17 @@ package com.Bridgelabz;
 import java.util.Scanner;
 
 public class LineCompComputation {
+
+    static void compareTo(Double line1, Double line2){
+        if (line1 == line2) {
+            System.out.println("Both the lines are equal in length.");
+        } else if (line1 > line2) {
+            System.out.println("Line 1 is greater than Line 2");
+        } else {
+            System.out.println("Line 2 is greater than Line 1");
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter X co-ordinator of 1st point: ");
@@ -14,8 +25,8 @@ public class LineCompComputation {
         System.out.println("Enter Y co-ordinator of 2nd point: ");
         int Y2 = sc.nextInt();
 
-        double len_of_line1 =(Math.sqrt(Math.pow((X2-X1),2)) + Math.pow((Y2-Y1) ,2));
-        System.out.println("Length of 1st end point is : "+len_of_line1);
+        double len_of_line1 = (Math.sqrt(Math.pow((X2 - X1), 2)) + Math.pow((Y2 - Y1), 2));
+        System.out.println("Length of 1st end point is : " + len_of_line1);
 
         System.out.println("Enter X co-ordinator of 1st point: ");
         int X3 = sc.nextInt();
@@ -26,13 +37,9 @@ public class LineCompComputation {
         System.out.println("Enter Y co-ordinator of 2nd point: ");
         int Y4 = sc.nextInt();
 
-        double len_of_line2 =(Math.sqrt(Math.pow((X4-X3),2)) + Math.pow((Y4-Y3) ,2));
-        System.out.println("Length of 2nd end point is : "+len_of_line2);
+        double len_of_line2 = (Math.sqrt(Math.pow((X4 - X3), 2)) + Math.pow((Y4 - Y3), 2));
+        System.out.println("Length of 2nd end point is : " + len_of_line2);
+        compareTo(len_of_line1, len_of_line2);
 
-        boolean checkEquality = len_of_line1.equals(len_of_line2);
-        if (checkEquality)
-            System.out.println("Both length of line are equal.");
-        else
-            System.out.println("Both length of line are not equal.");
     }
 }
